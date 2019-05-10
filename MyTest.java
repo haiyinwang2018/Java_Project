4e85434eadda54fd18c5a811c483d1cccde516e6
haiyinwang <870783273@qq.com>
2019-05-10 14:23:20 +0800

    this is java demo

---
+++ MyTest.java
@@ -0,0 +1,11 @@
+import java.awt.Frame;
+
+public class MyTest {
+    public static void main(String[] args) {
+        Frame w = new Frame();
+        w.setSize(1000,1500);
+        w.setLocation(40, 50);
+        w.setVisible(true);
+        
+    }
+}
